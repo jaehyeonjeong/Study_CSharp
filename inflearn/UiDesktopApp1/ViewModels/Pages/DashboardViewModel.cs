ce788640f83448a28bb20437f4c056db58e6013f
jaehyeonjeong <definejae234@gmail.com>
2025-12-02 12:11:11 +0900

    UI 데이터 바인딩

---
+++ inflearn/UiDesktopApp1/ViewModels/Pages/DashboardViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace UiDesktopApp1.ViewModels.Pages
+{
+    public partial class DashboardViewModel : ObservableObject
+    {
+        //기존 C# 변수는 내부에서 찍어야 함
+        private string? text;
+
+        public string MyProperty 
+        { 
+            get
+            {
+                return text;
+            }
+            set
+            {
+                this.text = value;
+                // 해당 변수가 changed 됨
+                // RaisePropertyChanged();
+            }
+        }
+
+        // 새로 생성한 Observalble Property
+        [ObservableProperty]
+        private string? _text01 = string.Empty;      // 이러면 제너릭이 자동으로 생성됨
+        // ?를 붙이는 이유는 null을 허용
+
+        // Integer 속성의 Observable Property 파라미터
+        [ObservableProperty]
+        private int _counter = 0; // 파라미터 대로 변수를 선언하면
+
+        [RelayCommand]
+        private void OnCounterIncrement()
+        {
+            //Counter++;  // 카운터 속성을 통해 변수에 접근(어디에?)
+            // 속성코드가 자동으로 제너레이션 된다.
+            //this.Text01 = "test";        // 자동 생성된 변수를 이런 방법으로 사용할 수 있음
+            this.Text01 = "Clicked";
+        }
+        // 이렇게하면 속성을 일일히 생성할 필요는 없어진다.
+    }
+}
---
+++ inflearn/UiDesktopApp1/Views/Pages/DashboardPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using UiDesktopApp1.ViewModels.Pages;
+using Wpf.Ui.Abstractions.Controls;
+
+namespace UiDesktopApp1.Views.Pages
+{
+    // xaml뷰와 뷰 모델은 1 대 1 매칭
+    public partial class DashboardPage : INavigableView<DashboardViewModel>
+    {
+        public DashboardViewModel ViewModel { get; }        // 데이터 바인딩을 할 수 있는 변수
+
+        public DashboardPage(DashboardViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            DataContext = this;
+
+            InitializeComponent();
+        }
+    }
+}
