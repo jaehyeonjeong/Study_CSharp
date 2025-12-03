6e89fbf779af7dd9b154b544ee60b2fddaab2347
jaehyeonjeong <definejae234@gmail.com>
2025-12-03 13:54:50 +0900

    사용자 정의 컨트롤 추가 - 로딩 컨트롤 추가 (사용자 정의) - 컨트롤 처리를 위한 뷰 모델 비동기 처리 구문 추가(Thread)

--- inflearn/UiDesktopApp1/ViewModels/Pages/DataViewModel.cs
+++ inflearn/UiDesktopApp1/ViewModels/Pages/DataViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Media;
+﻿using System.Threading.Tasks;
+using System.Windows.Media;
 using UiDesktopApp1.Interface;
 using UiDesktopApp1.Models;
 using Wpf.Ui.Abstractions.Controls;
@@ -33,7 +34,8 @@ namespace UiDesktopApp1.ViewModels.Pages
         public Task OnNavigatedToAsync()
         {
             if (!_isInitialized)
-                InitializeViewModel();
+                //InitializeViewModel();
+                InitializeViewModelAsync();
 
             return Task.CompletedTask;
         }
@@ -46,13 +48,22 @@ namespace UiDesktopApp1.ViewModels.Pages
             var selectedData = this.SelectedAdministrativeAgency;
         }
 
-        private void InitializeViewModel()
+        private async Task InitializeViewModelAsync()    // async Task를 붙여 비동기 식으로 변경 
         {
             // DB 전체 데이터 베이스 테이블 데이터 호출
-            this.GangnamguPopulations = this._population?.GetDataBaseTable();
+            //this.GangnamguPopulations = this._population?.GetDataBaseTable();
 
             // 제공 쿼리 select 로 AdministrativeAgency 데이터만 추출
-            this.AdministrativeAgency = this.GangnamguPopulations?.Select(c => c?.AdministrativeAgency).ToList();
+            //this.AdministrativeAgency = this.GangnamguPopulations?.Select(c => c?.AdministrativeAgency).ToList();
+
+            // 비동기로 데이터를 가져오기 (Thread 생성 후 비동기 실행)
+            this.GangnamguPopulations = await Task.Run(() => this._population?.GetDataBaseTable());
+
+            // 가져온 데이터를 가지고 필요한 작업을 수행
+            if (this.GangnamguPopulations != null)
+            {
+                this.AdministrativeAgency = this.GangnamguPopulations.Select(c => c?.AdministrativeAgency).ToList();
+            }
 
             _isInitialized = true;
         }
--- inflearn/UiDesktopApp1/Views/Pages/DataPage.xaml.cs
+++ inflearn/UiDesktopApp1/Views/Pages/DataPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using UiDesktopApp1.ViewModels.Pages;
+﻿using System.ComponentModel;
+using UiDesktopApp1.ViewModels.Pages;
 using Wpf.Ui.Abstractions.Controls;
 
 namespace UiDesktopApp1.Views.Pages
@@ -12,7 +13,24 @@ namespace UiDesktopApp1.Views.Pages
             ViewModel = viewModel;
             DataContext = this;
 
+            // 뷰 모델에서 프로퍼티 체인지 이벤트 처리
+            ViewModel.PropertyChanging += ViewModel_PropertyChanged;
             InitializeComponent();
+        }
+
+        private void ViewModel_PropertyChanged(object? sender, PropertyChangingEventArgs e)
+        {
+            switch (e.PropertyName) // DataViewModel에서 정의한 멤버
+            {
+                case "AdministrativeAgency":    // 콤보박스 데이터
+                    this.searchGridLoadingControl.Visibility = Visibility.Collapsed; // 로딩 컨트롤 속성 이름
+                    this.searchGrid.Visibility = Visibility.Visible;  // 가시성은 visible
+                    break;
+                case "GangnamguPopulations":    // 테이블 데이터
+                    this.dgGridLoadingControl.Visibility = Visibility.Collapsed;
+                    this.dgGrid.Visibility = Visibility.Visible;
+                    break;
+            }
         }
 
         private void cbxAdminAgency_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
