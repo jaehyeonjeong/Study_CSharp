1236909abbee08d1747f1f0add367abd012d8423
jaehyeonjeong <definejae234@gmail.com>
2025-12-04 10:45:12 +0900

    2일차 - MDI, 로그인 동작 익히기

---
+++ inflearn/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 폼 2를 보여주는 동작
+            Form2 form2 = new Form2();
+            form2.MdiParent = this; // form2의 부모창은 Form1
+            form2.Show();       // MDI 설정을 하면 Form1안에서만 이동가능, 클릭 시 복수의 form2를 생성
+            // MDI 멀티파트 도큐먼트 역할
+        }
+    }
+}
---
+++ inflearn/WindowsFormsApp2/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; 
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 알림창 띄우기
+            MessageBox.Show(123 + "매개변수가 한계입니다.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 로그인 알림
+            DialogResult dialogResult =  MessageBox.Show("로그인 하시겠습니까?", "로그인", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            // MessageBox 속성을 바꿀 수 있는 Enum 데이터로 속성 변경
+
+            // 사용자가 확인을 눌렀을 때 if문
+            if (DialogResult.OK == dialogResult)
+            {
+                // 사용자가 asdf로그인을 하였습니다. (메세지 박스)
+                if(textBox1.Text.Equals("asdf"))
+                {
+                    MessageBox.Show("로그인이 성공적으로 되었습니다. ID : " + textBox1.Text); // 아이디 정보 출력
+                }
+                else
+                {
+                    MessageBox.Show("로그인을 실패하셨습니다. ID : " + textBox1.Text);
+                }
+            }
+        }
+    }
+}
