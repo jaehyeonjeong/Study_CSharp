9032aec88937b0e770bc94aea967c96c85979404
jaehyeonjeong <definejae234@gmail.com>
2025-12-04 13:44:52 +0900

    2일차 - 이벤트 핸들러에 대한 이해 및 정리

--- inflearn/WindowsFormsApp6/Form1.cs
+++ inflearn/WindowsFormsApp6/Form1.cs
@@ -16,5 +16,23 @@ namespace WindowsFormsApp6
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("message box1");
+        }
+
+        //private int button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("message box2");
+            //return 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Click += new EventHandler(button1_Click);       // 나중에 추가되는 이벤트
+            // 왜냐.. Designer.cs에서 먼저 선언된 이벤트 설정 후 그 다음에 설정되기 때문 
+        }
     }
 }
